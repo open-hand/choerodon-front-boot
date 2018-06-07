0a6aef273e1e32f125a8871cd0e924d18479ec2e
2689 <99571603@qq.com>
2018-06-08 06:40:02 +0800

    [IMP]增加综合打包脚本

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const nunjucks = require('nunjucks');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const getChoerodonConfig = require('./config/getChoerodonConfig');
+const runCmd = require('./runCmd');
 const context = require('./context');
 
 const entryTemplate = fs.readFileSync(path.join(__dirname, 'entry.nunjucks.js')).toString();
@@ -22,15 +23,27 @@ function escapeWinPath(path) {
   return path.replace(/\\/g, '\\\\');
 }
 
-function getRoutesPath(configRoutes, configEntryName) {
-  if (!configRoutes || !configRoutes.length) {
-    const packageInfo = require(path.join(process.cwd(), 'package.json'));
-    if (packageInfo) {
-      const { main, name } = packageInfo;
-      configRoutes = { [name.slice(name.lastIndexOf('-') + 1)]: path.join(process.cwd(), main) };
-    }
-  }
+function getPackagePath(base = '.') {
+  return path.join(process.cwd(), base, 'package.json');
+}
 
+function getPackageRoute(packageInfo, base = '.') {
+  if (packageInfo) {
+    const { main, name } = packageInfo;
+    return { [name.slice(name.lastIndexOf('-') + 1)]: path.join(base, main) };
+  }
+}
+
+function getRoutes(packageInfo) {
+  const { choerodonConfig } = context;
+  let configRoutes = choerodonConfig.routes;
+  if (!configRoutes) {
+    return getPackageRoute(packageInfo);
+  }
+  return configRoutes;
+}
+
+function getRoutesPath(configRoutes, configEntryName) {
   const routesPath = path.join(tmpDirPath, `routes.${configEntryName}.js`);
   nunjucks.configure(routesPath, {
     autoescape: false,
@@ -43,7 +56,7 @@ function getRoutesPath(configRoutes, configEntryName) {
           _reactRouterDom.Route,
           {
             path: '/${key}',
-            component: _asyncRouter2['default'](() => import('${escapeWinPath(configRoutes[key])}')),
+            component: _asyncRouter2['default'](() => import('${escapeWinPath(path.join(process.cwd(), configRoutes[key]))}')),
           }
         ),`
       )).join('\n'),
@@ -52,10 +65,10 @@ function getRoutesPath(configRoutes, configEntryName) {
   return routesPath;
 }
 
-function generateEntryFile(configRoutes, configEntryName, root) {
+function generateEntryFile(mainPackage, configEntryName, root) {
   const entryPath = path.join(tmpDirPath, `entry.${configEntryName}.js`);
   const routesPath = getRoutesPath(
-    configRoutes,
+    getRoutes(mainPackage),
     configEntryName,
   );
   fs.writeFileSync(
@@ -67,13 +80,29 @@ function generateEntryFile(configRoutes, configEntryName, root) {
   );
 }
 
-exports.start = function start(program) {
-  const configFile = path.join(process.cwd(), program.config || 'choerodon.config.js');
-  const choerodonConfig = getChoerodonConfig(configFile);
-  context.initialize({ choerodonConfig });
+function getDependenciesByModules(modules) {
+  const { choerodonConfig } = context;
+  const dependencies = {};
+  const routes = modules.reduce((obj, module) => {
+    const packageInfo = require(getPackagePath(module));
+    Object.assign(dependencies, packageInfo.dependencies);
+    return Object.assign(obj, getPackageRoute(packageInfo, module));
+  }, {});
 
+  if (!choerodonConfig.routes) {
+    choerodonConfig.routes = routes;
+  }
+  return dependencies;
+}
+
+function install(done) {
+  runCmd(process.platform === 'win32' ? 'npm.cmd' : 'npm', ['i'], done);
+}
+
+function run(mainPackage) {
+  const { choerodonConfig } = context;
   generateEntryFile(
-    choerodonConfig.routes,
+    mainPackage,
     choerodonConfig.entryName,
     '/',
   );
@@ -106,23 +135,13 @@ exports.start = function start(program) {
     choerodonConfig.port, '0.0.0.0',
     () => openBrowser(`http://localhost:${choerodonConfig.port}`),
   );
-};
+}
 
-
-exports.build = function build(program, callback) {
-  const configFile = path.join(process.cwd(), program.config || 'choerodon.config.js');
-  const choerodonConfig = getChoerodonConfig(configFile);
-  context.initialize({
-    choerodonConfig,
-    isBuild: true,
-  });
-  rimraf.sync(choerodonConfig.output);
-  mkdirp.sync(choerodonConfig.output);
-
-  const { entryName } = choerodonConfig;
+function dist(mainPackage) {
+  const { choerodonConfig } = context;
   generateEntryFile(
-    choerodonConfig.routes,
-    entryName,
+    mainPackage,
+    choerodonConfig.entryName,
     choerodonConfig.root,
   );
   const webpackConfig = updateWebpackConfig(getWebpackCommonConfig(), 'build');
@@ -152,3 +171,50 @@ exports.build = function build(program, callback) {
     }
   });
 };
+
+exports.start = function start(program) {
+  const configFile = path.join(process.cwd(), program.config || 'choerodon.config.js');
+  const choerodonConfig = getChoerodonConfig(configFile);
+  context.initialize({ choerodonConfig });
+  const mainPackagePath = getPackagePath();
+  const mainPackage = require(mainPackagePath);
+  if (program.args.length) {
+    mainPackage.dependencies = Object.assign(getDependenciesByModules(program.args), mainPackage.dependencies);
+    fs.writeFileSync(
+      mainPackagePath,
+      JSON.stringify(mainPackage),
+    );
+    install(() => {
+      run(mainPackage);
+    });
+  } else {
+    run(mainPackage);
+  }
+};
+
+
+exports.build = function build(program) {
+  const configFile = path.join(process.cwd(), program.config || 'choerodon.config.js');
+  const choerodonConfig = getChoerodonConfig(configFile);
+  context.initialize({
+    choerodonConfig,
+    isBuild: true,
+  });
+  rimraf.sync(choerodonConfig.output);
+  mkdirp.sync(choerodonConfig.output);
+  const mainPackagePath = getPackagePath();
+  const mainPackage = require(mainPackagePath);
+  if (program.args.length) {
+    mainPackage.dependencies = Object.assign(getDependenciesByModules(program.args), mainPackage.dependencies);
+    fs.writeFileSync(
+      mainPackagePath,
+      JSON.stringify(mainPackage),
+    );
+    install(() => {
+      dist(mainPackage);
+    });
+  } else {
+    dist(mainPackage);
+  }
+};
+
