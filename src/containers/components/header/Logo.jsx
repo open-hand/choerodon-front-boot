eb36e48e47171b551ea87806c3d37f33d0afdcd0
陈彬杰 <binjie.chen@hand-china.com>
2018-09-20 14:46:13 +0800

    [IMP] dashboard上主页链接调换

--- src/containers/components/header/Logo.jsx
+++ src/containers/components/header/Logo.jsx
@@ -3,6 +3,7 @@ import { Link, withRouter } from 'react-router-dom';
 import { inject, observer } from 'mobx-react';
 import { Button } from 'choerodon-ui';
 import { PREFIX_CLS } from '../../common/constants';
+import { dashboard } from '../../common';
 
 const prefixCls = `${PREFIX_CLS}-boot-header-logo`;
 
@@ -16,9 +17,19 @@ export default class Logo extends Component {
   };
 
   render() {
-    const { MenuStore, location } = this.props;
+    const { AppState, MenuStore, location } = this.props;
     const { pathname, search } = location;
     const menus = MenuStore.getMenuData;
+    let homePath = '/';
+    if (dashboard) {
+      const { type, id, name, organizationId } = AppState.currentMenuType;
+      if (type && type !== 'site') {
+        homePath = `${homePath}?type=${type}&id=${id}&name=${name}`;
+        if (organizationId) {
+          homePath += `&organizationId=${organizationId}`;
+        }
+      }
+    }
     return (
       <div className={`${prefixCls}-wrap`}>
         {
@@ -29,7 +40,7 @@ export default class Logo extends Component {
         {
           pathname === '/' && !search
             ? <div className={prefixCls} />
-            : <Link to="/" className={prefixCls} />
+            : <Link to={homePath} className={prefixCls} />
         }
       </div>
     );
