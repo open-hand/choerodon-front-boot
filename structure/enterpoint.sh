8b2df3e081f3a6db26c8b9f06f9e0d057e4e07b6
binjie chen <binjie.chen@hand-china.com>
2019-04-04 17:01:43 +0800

    [FIX] 修复拼写错误

--- structure/enterpoint.sh
+++ structure/enterpoint.sh
@@ -10,7 +10,7 @@ find /usr/share/nginx/html -name '*.js' | xargs sed -i "s localhost:cookieServer
 find /usr/share/nginx/html -name '*.html' | xargs sed -i "s localhost:titlename $PRO_TITLE_NAME g"
 find /usr/share/nginx/html -name '*.js' | xargs sed -i "s localhost:fileserver $PRO_FILE_SERVER g"
 find /usr/share/nginx/html -name '*.js' | xargs sed -i "s localhost:wsserver $PRO_WEBSOCKET_SERVER g"
-find /usr/share/nginx/html -name '*.js' | xargs sed -i "s localhost:apimgateway $PRO_WEBSOCKET_SERVER g"
+find /usr/share/nginx/html -name '*.js' | xargs sed -i "s localhost:apimgateway $PRO_APIM_GATEWAY g"
 
 exec "$@"
 
