63e4ed66bb1f48c5b13709334dc447b076bef5b3
laincarl <1047972853@qq.com>
2019-12-04 10:45:24 +0800

    [ADD]监听配置文件，自动重启

--- src/bin/start.js
+++ src/bin/start.js
@@ -2,17 +2,17 @@ import path from 'path';
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
 import openBrowser from 'react-dev-utils/openBrowser';
+import gaze from 'gaze';
 import context from './common/context';
 import generateTransfer from './common/generateTransfer';
 import handleGenerateEntry from './common/generateEntry';
 import generateWebpackConfig from './common/generateWebpackConfig';
 import generateEnvironmentVariable from './common/generateEnvironmentVariable';
 
-export default function start(program, dev) {
+function restart(program, dev, open = false) {
   // 初始化全局参数context
   const { initContext } = context;
   initContext(program, dev);
-
   // 前端环境变量方案处理
   generateEnvironmentVariable(true);
 
@@ -46,6 +46,28 @@ export default function start(program, dev) {
   const server = new WebpackDevServer(compiler, serverOptions);
   server.listen(
     port, '0.0.0.0',
-    () => openBrowser(`http://localhost:${port}`),
+    () => {
+      if (open) {
+        openBrowser(`http://localhost:${port}`);
+      }
+    },
   );
+  return server;
+}
+export default function start(program, dev) {
+  let server = restart(program, dev, true);
+  const configFiles = [program.config, './react/.env', './react/.default.env'].map((file) => path.join(process.cwd(), file));
+  gaze(configFiles, (err, watcher) => {
+    // Files have all started watching
+    // Get all watched files
+    const watched = watcher.watched();
+    watcher.on('all', (event, filepath) => {
+      const filename = path.basename(filepath);
+      // eslint-disable-next-line no-console
+      console.log(`Since ${filename} was ${event}, try to restart server...`);
+      server.close(() => {
+        server = restart(program, dev);
+      });
+    });
+  });
 }
