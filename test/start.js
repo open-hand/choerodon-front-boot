a8a0e8792916a6d140f65ad091cd5d52350ff1fb
isaac <273070111@qq.com>
2020-05-08 10:16:56 +0800

    [FIX]修改本地npm start报错问题

--- test/start.js
+++ test/start.js
@@ -1,3 +1,9 @@
+const program = require('commander');
+
+program
+  .option('-c, --config <path>', 'set config path. defaults to ./choerodon.config.js')
+  .parse(process.argv);
+
 const start = require('../lib/bin/start').default;
 
-start({}, true);
+start(program, true);
