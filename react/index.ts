bebd6b5984e7b0a10c6628d50ae8846af8e6b8d6
laincarl <1047972853@qq.com>
2021-04-07 09:34:29 +0800

    [DEL]先注释类型

--- react/index.ts
+++ react/index.ts
@@ -1,21 +1,21 @@
-import type { Configuration } from 'webpack';
+// import type { Configuration } from 'webpack';
 
-/** boot启动配置 */
-interface Config {
-  /** 页面入口 */
-  entry?: string
-  /** 要运行的子模块, ['.']代表当前模块 */
-  modules: string[]
-  /** 打包目录 */
-  distBasePath?: string
-  /** webpack的output */
-  output?: string
-  /** html模板title */
-  titlename?: string
-  /** 覆盖webpack配置 */
-  webpackConfig?: (config: Configuration) => Configuration
-}
+// /** boot启动配置 */
+// interface Config {
+//   /** 页面入口 */
+//   entry?: string
+//   /** 要运行的子模块, ['.']代表当前模块 */
+//   modules: string[]
+//   /** 打包目录 */
+//   distBasePath?: string
+//   /** webpack的output */
+//   output?: string
+//   /** html模板title */
+//   titlename?: string
+//   /** 覆盖webpack配置 */
+//   webpackConfig?: (config: Configuration) => Configuration
+// }
 
-export function createConfig(config: Config): Config {
-  return config;
-}
+// export function createConfig(config: Config): Config {
+//   return config;
+// }
