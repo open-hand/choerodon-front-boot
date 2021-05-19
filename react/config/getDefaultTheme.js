123978497208fd7361c7d0a58257130a90fe1e9f
20399 <yanfang.lin@hand-china.com>
2021-05-19 16:08:53 +0800

    [IMP] update defaultTheme

--- react/config/getDefaultTheme.js
+++ react/config/getDefaultTheme.js
@@ -2,5 +2,8 @@ export default function defaultTheme(env) {
   return {
     'primary-color': '#3f51b5',
     'c7n-prefix': 'c7n',
+    'text-color': '0f1358',
+    'help-color': 'rgb(171, 188, 207)',
+    'divider-color': '#d9e6f2',
   };
 }
