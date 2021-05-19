2b3a8b4b956895e4bd919bd3773952f7b7f90698
20399 <yanfang.lin@hand-china.com>
2021-05-19 18:44:25 +0800

    [IMP] update text-color

--- react/config/getDefaultTheme.js
+++ react/config/getDefaultTheme.js
@@ -2,8 +2,6 @@ export default function defaultTheme(env) {
   return {
     'primary-color': '#3f51b5',
     'c7n-prefix': 'c7n',
-    'text-color': '0f1358',
-    'help-color': 'rgb(171, 188, 207)',
-    'divider-color': '#d9e6f2',
+    'text-color': '#0f1358',
   };
 }
