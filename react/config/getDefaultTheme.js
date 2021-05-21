dd90826359def74e149ccd03bb02250e7901e143
laincarl <1047972853@qq.com>
2021-05-21 15:18:48 +0800

    [FIX]修改颜色覆盖

--- react/config/getDefaultTheme.js
+++ react/config/getDefaultTheme.js
@@ -1,7 +1,8 @@
 export default function defaultTheme(env) {
   return {
-    'primary-color': '#3f51b5',
+    'primary-color': '#5365EA',
     'c7n-prefix': 'c7n',
     'text-color': '#0f1358',
+    'input-color': '#0f1358',
   };
 }
