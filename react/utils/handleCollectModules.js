672367af8f5bed156712907e09843dae07b3fa51
翁恺敏 <736653759@qq.com>
2021-11-15 22:15:26 +0800

    [IMP] split the modules collection logics and add some configs

---
+++ react/utils/handleCollectModules.js
@@ -0,0 +1,15 @@
+import context from './context';
+import getPackagePath from './getPackagePath';
+import getPackageInstall from './getPackageInstall';
+
+export default function handleCollectModules() {
+  const { choerodonConfig, choerodonConfig: { modules } } = context;
+  const installs = modules.reduce((res, module) => {
+    const packageInfo = require(getPackagePath(module));
+    return [...res, getPackageInstall(packageInfo, module)];
+  }, []).filter(Boolean);
+
+  if (!choerodonConfig.installs) {
+    choerodonConfig.installs = installs;
+  }
+}
