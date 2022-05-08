bd84bd821bab3d8ffb61a83522ba201f723faa02
isaac <273070111@qq.com>
2022-05-08 16:23:35 +0800

    [ADD]

--- react/config/webpack.config.js
+++ react/config/webpack.config.js
@@ -109,6 +109,11 @@ export default function getWebpackCommonConfig (mode, env, envStr) {
             priority: 10,
             chunks: 'initial', // 只打包初始时依赖的第三方
           },
+          ckeditor: {
+            name: 'chunk-ckeditor',
+            priority: 20,
+            test: /[\\/]node_modules[\\/]@choerodon\/ckeditor[\\/]/,
+          },
           choerodonUI: {
             name: 'chunk-ui', // 单独将 UI 拆包
             priority: 20, // 权重要大于 libs 和 app 不然会被打包进 libs 或者 app
