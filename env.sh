ffc5aace928c2149439f95332ce659eb503a5f13
钟梓凌 <setzero@zknow.com>
2022-05-19 16:31:29 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -31,22 +31,24 @@ do
   echo "  $varname: \"$value\"," >> ${env_config}
 done < ${absolute_path}/.env
 
-while read -r line || [[ -n "$line" ]];
-do
-  # Split env variables by character `=`
-  if printf '%s\n' "$line" | grep -q -e '='; then
-    varname=$(printf '%s\n' "$line" | sed -e 's/=.*//')
-    varvalue=$(printf '%s\n' "$line" | sed -e 's/^[^=]*=//')
-  fi
+if [ -f "${absolute_path}/.default.env" ]; then
+  while read -r line || [[ -n "$line" ]];
+  do
+    # Split env variables by character `=`
+    if printf '%s\n' "$line" | grep -q -e '='; then
+      varname=$(printf '%s\n' "$line" | sed -e 's/=.*//')
+      varvalue=$(printf '%s\n' "$line" | sed -e 's/^[^=]*=//')
+    fi
 
-  # Read value of current variable if exists as Environment variable
-  value=$(printf '%s\n' "${!varname}")
-  # Otherwise use value from .env file
-  [[ -z $value ]] && value=${varvalue}
+    # Read value of current variable if exists as Environment variable
+    value=$(printf '%s\n' "${!varname}")
+    # Otherwise use value from .env file
+    [[ -z $value ]] && value=${varvalue}
 
-  # Append configuration property to JS file
-  echo "  $varname: \"$value\"," >> ${env_config}
-done < ${absolute_path}/.default.env
+    # Append configuration property to JS file
+    echo "  $varname: \"$value\"," >> ${env_config}
+  done < ${absolute_path}/.default.env
+fi
 
 echo "}" >> ${env_config}
 sed -e "s/\//\\\/g" ${env_config}
